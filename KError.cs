e880a79e341e9c738a17cd02fda669e47ace32bc
Greg <gregoire.rit@gmail.com>
2012-06-25 17:10:55 +0200

    KinectSensor.Status

---
+++ KError.cs
@@ -0,0 +1,8 @@
+﻿namespace KinectServer
+{
+    static class KError
+    {
+        public const byte GetSensorCollectionError = 101;
+        public const byte StatusKinectSensorError = 102; 
+    }
+}
